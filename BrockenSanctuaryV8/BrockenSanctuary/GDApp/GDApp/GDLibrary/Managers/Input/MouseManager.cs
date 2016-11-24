794bcb73548f804130af21075f9bd93f8d150835
Patrick Collins <d00168823@student.dkit.ie>
2016-11-24 20:23:21 +0000

    starting repo with version 8 of BS (hee hee)
    BrockenSanctuary(V8)

---
+++ BrockenSanctuaryV8/BrockenSanctuary/GDApp/GDApp/GDLibrary/Managers/Input/MouseManager.cs
@@ -0,0 +1,240 @@
+﻿/*
+Function: 		Provide mouse input functions
+Author: 		NMCG
+Version:		1.0
+Date Updated:	26/9/16
+Bugs:			None
+Fixes:			None
+*/
+
+using GDApp;
+using JigLibX.Collision;
+using JigLibX.Geometry;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace GDLibrary
+{
+    /// <summary>
+    /// Provides methods to determine the state of the mouse.
+    /// </summary>
+    /// 
+
+    //used for ray picking
+    class ImmovableSkinPredicate : CollisionSkinPredicate1
+    {
+        public override bool ConsiderSkin(CollisionSkin skin0)
+        {
+            if (skin0.Owner != null)
+                return true;
+            else
+                return false;
+        }
+    }
+    public class MouseManager : GameComponent
+    {
+        #region Variables
+        private Main game;
+        private MouseState newState, oldState;
+        #endregion
+
+        #region Properties
+        public Microsoft.Xna.Framework.Rectangle Bounds
+        {
+            get
+            {
+                return new Microsoft.Xna.Framework.Rectangle(this.newState.X, this.newState.Y, 1, 1);
+            }
+        }
+        public bool IsVisible
+        {
+            get
+            {
+                return game.IsMouseVisible;
+            }
+            set
+            {
+                game.IsMouseVisible = value;
+            }
+        }
+        public Vector2 Position
+        {
+            get
+            {
+                return new Vector2(this.newState.X, this.newState.Y);
+            }
+        }
+        #endregion
+
+        public MouseManager(Main game, bool isVisible)
+            : base(game)
+        {
+
+            this.game = game;
+            game.IsMouseVisible = isVisible;
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+     
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            //store the old state
+            this.oldState = newState;
+
+            //get the new state
+            this.newState = Mouse.GetState();
+
+            base.Update(gameTime);
+        }
+
+        public bool HasMoved()
+        {
+            float deltaPositionLength = new Vector2(newState.X - oldState.X, 
+                newState.Y - oldState.Y).Length();
+
+            return (deltaPositionLength > AppData.MouseSensitivity) ? true : false;
+        }
+        public bool IsLeftButtonClickedOnce()
+        {
+            return ((newState.LeftButton.Equals(ButtonState.Pressed)) && (!oldState.LeftButton.Equals(ButtonState.Pressed)));
+        }
+        public bool IsLeftButtonClicked()
+        {
+            return (newState.LeftButton.Equals(ButtonState.Pressed));
+        }
+        public bool IsRightButtonClickedOnce()
+        {
+            return ((newState.RightButton.Equals(ButtonState.Pressed)) && (!oldState.RightButton.Equals(ButtonState.Pressed)));
+        }
+        public bool isRightButtonClicked()
+        {
+            return (newState.RightButton.Equals(ButtonState.Pressed));
+        }
+
+        //Calculates the mouse pointer distance (in X and Y) from a user-defined position
+        public Vector2 GetDeltaFromPosition(Vector2 position)
+        {
+            return new Vector2(this.newState.X - position.X, 
+            this.newState.Y - position.Y);
+        }
+
+        //Calculates the mouse pointer distance from the screen centre
+        public Vector2 GetDeltaFromCentre()
+        {
+            return new Vector2(this.newState.X - this.game.ScreenCentre.X,
+                        this.newState.Y - this.game.ScreenCentre.Y);
+        }
+
+        //has the mouse state changed since the last update?
+        public bool IsStateChanged()
+        {
+            return (this.newState.Equals(oldState)) ? false : true;
+        }
+
+        //how much has the scroll wheel been moved since the last update?
+        public int GetDeltaFromScrollWheel()
+        {
+            if (IsStateChanged()) //if state changed then return difference
+                return newState.ScrollWheelValue - oldState.ScrollWheelValue;
+
+            return 0;
+        }
+
+        public void SetPosition(Vector2 position)
+        {
+            Mouse.SetPosition((int)position.X, (int)position.Y);
+        }
+
+        #region Ray Picking
+        //get a ray positioned at the mouse's location on the screen - used for picking 
+        public Microsoft.Xna.Framework.Ray GetMouseRay(Camera3D camera)
+        {
+            //get the positions of the mouse in screen space
+            Vector3 near = new Vector3(this.newState.X, this.Position.Y, 0);
+
+            //convert from screen space to world space
+            near = camera.Viewport.Unproject(near, camera.ProjectionParameters.Projection, camera.View, Matrix.Identity);
+
+            return GetMouseRayFromNearPosition(camera, near);
+        }
+
+        //get a ray from a user-defined near position in world space and the mouse pointer
+        public Microsoft.Xna.Framework.Ray GetMouseRayFromNearPosition(Camera3D camera, Vector3 near)
+        {
+            //get the positions of the mouse in screen space
+            Vector3 far = new Vector3(this.newState.X, this.Position.Y, 1);
+
+            //convert from screen space to world space
+            far = camera.Viewport.Unproject(far, camera.ProjectionParameters.Projection, camera.View, Matrix.Identity);
+
+            //generate a ray to use for intersection tests
+            return new Microsoft.Xna.Framework.Ray(near, Vector3.Normalize(far - near));
+        }
+
+        public Vector3 GetMouseRayDirection(Camera3D camera)
+        {
+            //get the positions of the mouse in screen space
+            Vector3 near = new Vector3(this.newState.X, this.Position.Y, 0);
+            Vector3 far = new Vector3(this.newState.X, this.Position.Y, 1);
+
+            //convert from screen space to world space
+            near = camera.Viewport.Unproject(near, camera.ProjectionParameters.Projection, camera.View, Matrix.Identity);
+            far = camera.Viewport.Unproject(far, camera.ProjectionParameters.Projection, camera.View, Matrix.Identity);
+
+            //generate a ray to use for intersection tests
+            return Vector3.Normalize(far - near);
+        }
+
+        float frac; CollisionSkin skin;
+        public Actor GetPickedObject(Camera3D camera, float distance, out Vector3 pos, out Vector3 normal)
+        {
+            Vector3 ray = GetMouseRayDirection(camera);
+            ImmovableSkinPredicate pred = new ImmovableSkinPredicate();
+
+            this.game.PhysicsManager.PhysicsSystem.CollisionSystem.SegmentIntersect(out frac, out skin, out pos, out normal,
+                new Segment(camera.Transform3D.Translation, ray * distance), pred);
+
+            if (skin != null && skin.Owner != null)
+            {
+                return skin.Owner.ExternalData as Actor;
+            }
+
+            return null;
+        }
+
+        //used when in 1st person collidable camera mode
+        //start distance allows us to start the ray outside the collidable skin of the 1st person colliable camera object
+        //otherwise the only thing we would ever collide with would be ourselves!
+        public Actor GetPickedObject(Camera3D camera, float startDistance, float distance,
+                   out Vector3 pos, out Vector3 normal)
+        {
+            Vector3 ray = GetMouseRayDirection(camera);
+            ImmovableSkinPredicate pred = new ImmovableSkinPredicate();
+
+            this.game.PhysicsManager.PhysicsSystem.CollisionSystem.SegmentIntersect(
+                out frac, out skin, out pos, out normal,
+                new Segment(camera.Transform3D.Translation + startDistance * Vector3.Normalize(ray), ray * distance), pred);
+
+            if (skin != null && skin.Owner != null)
+            {
+                return skin.Owner.ExternalData as Actor;
+            }
+
+            return null;
+        }
+        #endregion
+    }
+}
---
+++ BrockenSanctuaryV8/BrockenSanctuary/GDApp/GDApp/GDLibrary/Objects/3D/Base/DrawnActor3D.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace GDLibrary
+{
+    public class DrawnActor3D : Actor3D, ICloneable
+    {
+        #region Variables
+        private BasicEffect effect;
+        private Color color;
+        private float alpha;
+        #endregion
+
+        #region Properties
+        public BasicEffect Effect
+        {
+            get
+            {
+                return this.effect;
+            }
+            set
+            {
+                this.effect = value;
+            }
+        }
+        public Color Color
+        {
+            get
+            {
+                return this.color;
+            }
+            set
+            {
+                this.color = value;
+            }
+        }
+        public float Alpha
+        {
+            get
+            {
+                return this.alpha;
+            }
+            set
+            {
+                this.alpha = value;
+            }
+        }
+        #endregion
+
+        //used when we don't want to specify color and alpha
+        public DrawnActor3D(string id, ActorType actorType,
+         Transform3D transform, BasicEffect effect)
+            : this(id, actorType, transform, effect, 
+            Color.White, 1, 
+            StatusType.Drawn | StatusType.Updated) //when we bitwise OR we saw drawn AND updated
+        {
+        }
+
+        //forward compatibility (since v3.4) for existing code with no StatusType
+        public DrawnActor3D(string id, ActorType actorType,
+           Transform3D transform, BasicEffect effect, Color color, float alpha)
+            : this(id, actorType, transform, effect, color, alpha, 
+            StatusType.Drawn | StatusType.Updated) //when we bitwise OR we saw drawn AND updated
+
+        {
+
+        }
+
+        public DrawnActor3D(string id, ActorType actorType,
+            Transform3D transform, BasicEffect effect, Color color, 
+            float alpha, StatusType statusType)
+            : base(id, actorType, transform, statusType)
+        {
+            this.effect = effect;
+            this.color = color;
+            this.alpha = alpha;
+        }
+
+        public new object Clone()
+        {
+            return new DrawnActor3D("clone - " + ID, //deep
+                this.ActorType, //deep
+                (Transform3D)this.Transform3D.Clone(), //deep
+                this.effect, //shallow - its ok if objects refer to the same effect
+                this.color, //deep
+                this.alpha); //deep
+        }
+
+        //add remove...
+    }
+}
